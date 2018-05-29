86bc87f3f77bfa559a9a337a19c288ea19b7888f
pamelars86 <pamela.rosales@pucp.pe>
2018-05-29 14:45:31 -0300

    Criação da pasta de componentes

---
+++ src/components/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div className="">
+
+
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
