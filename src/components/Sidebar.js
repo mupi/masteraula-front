03a1b71254364da340b8cc46980a35006d71856a
Daniela Palumbo <d166301@dac.unicamp.br>
2018-06-07 14:37:34 -0300

    Chenged sidebar color

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,16 +11,16 @@ import UserProfilePage from "./UserProfilePage.js";
 
 class Sidebar extends Component {
   render() {
-    let border = {'border': 'black solid 2px'}
+    let border = {'border': 'white solid 1px'}
     return (
             <ListGroup >
-              <ListGroupItem style={border} tag="a" action href='/new-document'>
+              <ListGroupItem color='secondary' style={border} tag="a" action href='/new-document'>
                 Novo Documento
               </ListGroupItem>
-              <ListGroupItem style={border} tag="a" action href='/documents'>
+              <ListGroupItem color='secondary' style={border} tag="a" action href='/documents'>
                 Meus Documentos
               </ListGroupItem>
-              <ListGroupItem style={border} tag="a" action href='/home'>
+              <ListGroupItem color='secondary' style={border} tag="a" action href='/home'>
                 Banco de questões
               </ListGroupItem>
             </ListGroup>
