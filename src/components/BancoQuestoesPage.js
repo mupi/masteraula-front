ec02562ef7e32b19aac81fdfcde07b1c2dc99a54
pamelars86 <pamela.rosales@pucp.pe>
2018-06-11 16:00:09 -0300

    ultima versao

--- src/components/BancoQuestoesPage.js
+++ src/components/BancoQuestoesPage.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Row, Col, ListGroup, ListGroupItem, Input, InputGroup, InputGroupAddon, Button} from 'reactstrap';
-import QuestionCard from "./question/QuestionCard"
-class BancoQuestoesPage extends Component {
+import QuestionList from "./question/QuestionList"
 
-  constructor(props) {
-    super(props);
+const  results = {
+    "rquestions": [
+      { "disciplines": [
+        { "name": "Química" },
+        { "name": "Física" }
+      ],
+      "source": "ENEM",
+      "year": "2010",
+      "author": "Thiago Oliveira dos Santos",
+      "extract": "Assinale a alternativa que melhor expresse o efeito de humor contido na tirinha: a) O discurso feminista de Susanita é responsável pelo efeito de humor, já que o tema é tratado de forma irônica, denotando certo machismo ...",
+      "urlImage": ""
+      },
+      { "disciplines": [
+        { "name": "Matemática" },
+        { "name": "Geometria" }
+      ],
+      "source": "ENEM",
+      "year": "2018",
+      "author": "Diego Gonçalves Carvalho",
+      "extract": "Assinale a alternativa que melhor expresse o efeito de humor contido na tirinha: a) O discurso feminista de Susanita é responsável pelo efeito de humor, já que o tema é tratado de forma irônica, denotando certo machismo ...",
+      "urlImage": ""
+      }
+    ]
+
   }
 
-  render() {
-    return (
+
+
+const BancoQuestoesPage= () =>
             <Row>
               <Col xs='6'>
               Digite o termo e encontre soluções relacionadas
@@ -18,20 +40,9 @@ class BancoQuestoesPage extends Component {
                 <InputGroupAddon addonType="prepend"><Button>Pesquisar</Button></InputGroupAddon>
               </InputGroup>
               <Row style={{'margin-top':'10px'}}>
-              <Col>
-                <QuestionCard />
-                </Col>
-                <Col>
-                <QuestionCard />
-                </Col>
-                <Col>
-                <QuestionCard />
-                </Col>
-              </Row>
+                  <QuestionList questions={results.rquestions} />
+                </Row>
                </Col>
             </Row>
-    );
-  }
-}
 
 export default BancoQuestoesPage;
