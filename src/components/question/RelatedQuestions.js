5849810c0d5f3ee078f8f7d7b025d4503e46dc72
pamelars86 <pamela.rosales@pucp.pe>
2018-06-11 09:03:51 -0300

    ultima versão do front, inclui os componentes da Pamela e da Dani

--- src/components/question/RelatedQuestions.js
+++ src/components/question/RelatedQuestions.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import 'font-awesome/css/font-awesome.min.css';
-import QuestionAvatar from './QuestionAvatar'
 
 const RelatedQuestions = (rquestions)=>  (
               <Container>
                 <Row className="title-section-question">
                       <h4><i className="fa fa-clone"></i> Questões Relacionadas</h4>
                 </Row>
-                
+
               </Container>
   )
 
