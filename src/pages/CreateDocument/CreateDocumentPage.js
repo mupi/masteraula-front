29d388b77d78645f045f123c00e19af5bc8d4abc
pamelars86 <pamela.rosales@pucp.pe>
2018-06-21 13:55:25 -0300

    CreateDocument Page was fixed

--- src/pages/CreateDocument/CreateDocumentPage.js
+++ src/pages/CreateDocument/CreateDocumentPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Container, Nav, NavItem, NavLink, TabContent, TabPane, Col } from 'reactstrap';
+import { Row, Container, Nav, NavItem, NavLink, TabContent, TabPane, Col, Button, Card, CardTitle, CardText } from 'reactstrap';
 import DocumentForm from '../../components/document/DocumentForm.js';
 import DocumentPreview from '../../components/document/DocumentPreview.js';
 import HomeUserPage from "../HomeUser/HomeUserPage"
@@ -29,36 +29,38 @@ class CreateDocumentPage extends Component{
                 'studentName':true, 'date':true, 'class':true, 'grade':true}
     return(
           <HomeUserPage>
-            <Container>
-              <Row>
-              	<Nav tabs>
-                <NavItem>
-                  <NavLink
-                    className={classnames({ active: this.state.activeTab === '1' })}
-                    onClick={() => { this.toggle('1'); }}
-                  >
-                    Monte seu documento
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={classnames({ active: this.state.activeTab === '2' })}
-                    onClick={() => { this.toggle('2'); }}
-                   >
-                    Preview
-                  </NavLink>
-                </NavItem>
-              </Nav>
-              <TabContent activeTab={this.state.activeTab} style={{'display':'block'}}>
-                <TabPane tabId="1">
-                      <DocumentForm />
-                </TabPane>
-                <TabPane tabId="2">
-                      <DocumentPreview data={data}/>
-                </TabPane>
-                </TabContent>
-              </Row>
-            </Container>
+            <div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '1' })}
+              onClick={() => { this.toggle('1'); }}
+            >
+              Monte seu documento
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '2' })}
+              onClick={() => { this.toggle('2'); }}
+            >
+             Preview
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <DocumentForm />
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+               <DocumentPreview data={data}/>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
           </HomeUserPage>);
     }
 }
