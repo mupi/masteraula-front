c9b57d4db82aa1beb8d87d77ed63a79e9bd07868
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-06-26 12:30:48 -0300

    Add profileEdit to store

--- src/Store.js
+++ src/Store.js
@@ -3,7 +3,7 @@ import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import { combineReducers } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
-import { login, menu, register, forgotPassword } from 'reducers'
+import { login, menu, register, forgotPassword, profileEdit } from 'reducers'
 import { history } from 'helpers/history'
 import { reducer as formReducer } from 'redux-form'
 
@@ -14,6 +14,7 @@ const rootReducer = combineReducers({
     menu,
     register,
     forgotPassword,
+    profileEdit,
     form: formReducer
 })
 
