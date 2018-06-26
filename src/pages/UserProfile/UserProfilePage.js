38259d88ccb2a8c9adcf7942b75dc5b3efb80b64
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-06-26 09:58:50 -0300

    Add redux-form to profile edit

--- src/pages/UserProfile/UserProfilePage.js
+++ src/pages/UserProfile/UserProfilePage.js
@@ -6,12 +6,12 @@ import UserProfile from "components/userprofile/UserProfile.js";
 import 'assets/css/UserProfile.css';
 
 const UserProfilePage = () =>
-          <HomeUserPage>
-                <div className="contenedor-profile">
-                    <h3 className="text-center">Meu Profile</h3>
-                    <h5 className="text-center">Permite que a comunidade do MasterAula te conheça</h5>
-                    <UserProfile />
-                </div>
-          </HomeUserPage>
+    <HomeUserPage>
+        <div className="contenedor-profile">
+            <h3 className="text-center">Meu Profile</h3>
+            <h5 className="text-center">Permite que a comunidade do MasterAula te conheça</h5>
+            <UserProfile />
+        </div>
+    </HomeUserPage>
 
 export default UserProfilePage;
