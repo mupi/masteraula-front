11ae69aa25f10e3fd21e8f1c5eef86e61de0bb20
pamelars86 <pamela.rosales@pucp.pe>
2018-06-29 12:00:24 -0300

    termuse page was added

---
+++ src/pages/TermsUse/TermsUsePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/TermsUse.css';
+
+
+const TermsUsePage = () =>
+    <div className="main-contenedor middle-box text-center loginscreen  animated fadeInDown">
+        <div className="contenedor-login">
+            <h3>Entrar no MasterAula</h3>
+          
+        </div>
+    </div>
+
+export default TermsUsePage;
