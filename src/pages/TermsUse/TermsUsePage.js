ba2282654ea0d5ce8107ec0a23dad55f053505f3
pamelars86 <pamela.rosales@pucp.pe>
2018-06-29 12:01:10 -0300

    terms use page was added

--- src/pages/TermsUse/TermsUsePage.js
+++ src/pages/TermsUse/TermsUsePage.js
@@ -7,7 +7,45 @@ const TermsUsePage = () =>
     <div className="main-contenedor middle-box text-center loginscreen  animated fadeInDown">
         <div className="contenedor-login">
             <h3>Entrar no MasterAula</h3>
-          
+            Damos boas-vindas ao Portal MasterAula.
+            O Portal MasterAula é mantido pela MUPI e nossa missão é apoiar o trabalho de educadores através da oferta de formação continuada online e de conteúdos que acreditamos serem válidos para esses fins.
+
+            Ao usar nosso Portal, você está concordando com estes termos e com nossa política de privacidade. Leia-os com atenção. Este documento contém INFORMAÇÕES LEGAIS importantes e indispensáveis à utilização do Portal.
+
+            SOBRE O CONTEÚDO DO PORTAL E O ACESSO AO MESMO
+            Os conteúdos e as informações contidas neste site poderão ser atualizadas ou modificadas periodicamente. Por conseguinte, não devem ser interpretadas como definitivas.
+
+            SOBRE A CONTA E O CADASTRO DO USUÁRIO
+            Para utilizar o Portal você precisa criar uma Conta pessoal e intransferível através de endereço de email válido e de sua posse e de senha pessoal e confidencial. Caso tenha conhecimento de qualquer uso não autorizado de sua senha ou conta, o usuário deverá imediatamente entrar em contato através do canal contato@mupi.me.
+
+            SOBRE LINKS EXTERNOS
+            A MUPI não se responsabiliza pela navegação dos USUÁRIOS nos links externos contidos nos conteúdos dos cursos e tampouco pela disponibilidade dos mesmos, uma vez que a equipe da MUPI não é responsável pela manutenção nem pelo conteúdo de tais sites.
+
+            SOBRE CANCELAMENTO
+            A MUPI não efetua devolução de pagamento após o USUÁRIO ter realizado o mesmo, exceto quando a oferta do produto adquirido seja cancelada pela MUPI.
+
+            SOBRE OS CONTEÚDOS PRODUZIDOS PELOS USUÁRIOS
+            A MUPI poderá fazer uso, em qualquer um de seus produtos, de todo conteúdo produzido pelos INSCRITOS durante os cursos oferecidos, como em atividades, trabalhos e comentários nos espaços de discussão.
+
+            SOBRE PROPRIEDADE INTELECTUAL
+            O uso comercial da expressão Portal como marca, nome empresarial ou nome de domínio, bem como os conteúdos das telas do Portal, assim como os programas, bancos de dados, redes, arquivos que permitem que o USUÁRIO acesse e use sua conta são de propriedade da empresa responsável pelo Portal.
+
+            SOBRE OS DIREITOS DA PERSONALIDADE
+            Por meio deste instrumento o USUÁRIO autoriza a empresa responsável pelo Portal, ou terceiro à sua ordem, a utilizar seus direitos de personalidade, tais como imagem, nome, depoimento e voz, nos materiais de comunicação utilizados por ela, ou qualquer outro material de terceiro que tenha a participação e/ou patrocínio da mesma, para veiculação e divulgação na mídia em geral, escrita, falada, televisada ou eletrônica, de difusão e transmissão por qualquer meio de comunicação, dentre os quais se citam, sem exclusão de qualquer outro aqui não previsto, Ações de merchandising, adesivo, agenda, álbum, anúncios, aplicativos, apostilas, artigos de vestuário, backlight, banner, blog, boletins, brindes, busdoor, caderno, calendário, cartão, cartão postal, cartaz, catálogos, cd-rom, convites, cursos, display, e-mails, encarte, envelope, espetáculo, estande, etiqueta, eventos, faixas, feiras, filmes, flyers, folders, folheto, formulários, frontlight, home page, ilustração, internet, jornal, livreto, livro, mala direta, materiais de identidade visual,  materiais didáticos, mensagens para celular, mural, newsletters, obras audiovisuais, obras multimídias, outdoors, painel eletrônico, papel de carta, peças publicitárias impressas, sonoras ou audiovisuais, pôster, produtos, programa de computador, rádio, redes sociais, relatórios de qualquer natureza, revistas, ringtones, selo, seminários, still para tv, televisão, treinamentos, vídeos, press releases (divulgação para a imprensa), apresentações internas e externas, eventos institucionais internos ou externos, eventos ou materiais históricos da empresa, banco de dados, histórico de sites da empresa, intranet e quaisquer outros materiais e meios de comunicação que a empresa deseje utilizar para divulgação ao público interno e/ou externo, com finalidade institucional e/ou educacional.
+
+            SOBRE OS USOS DO USUÁRIO
+            O USUÁRIO compromete-se a não utilizar o serviço com a finalidade de armazenar, distribuir, transmitir, difundir, colocar à disposição de terceiros conteúdos que incluam mensagens, gráficos, desenhos, arquivos de som e/ou imagem, fotografias, gravações, em geral, qualquer classe de material que por si mesmo ou cuja transmissão para:
+
+            SOBRE A REMOÇÃO DE CONTEÚDO INDEVIDO
+            O Portal envidará seus melhores esforços para promover qualquer investigação interna visando a remoção de conteúdos, desde que a requerimento da parte lesada mediante apresentação dos motivos e indícios de que determinado conteúdo viole as leis ou os Termos e Condições de Uso do Site.
+
+            ALTERAÇÕES DOS TERMOS E CONDIÇÕES
+            Quaisquer alterações que impactem especificamente em ônus financeiro ao USUÁRIO serão feitas mediante comunicação prévia ao mesmo através de seu e-mail de cadastro, que poderá manifestar a sua concordância ou não.
+
+            SOBRE A LEI APLICÁVEL E JURISDIÇÃO
+            O presente Termo e Condições de Uso são regidos pela legislação da República Federativa do Brasil. Seu texto deverá ser interpretado no idioma português e os USUÁRIOS do Portal se submetem ao Foro Central da Comarca da cidade de Campinas do Estado de São Paulo.
+
+            Atualizado em: 9 de outubro de 2017
         </div>
     </div>
 
