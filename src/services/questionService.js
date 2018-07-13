c600693ac6e50a823c4228e1eac1320eb44fd406
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-07-12 23:14:05 -0300

    Add list question service

--- src/services/questionService.js
+++ src/services/questionService.js
@@ -2,20 +2,9 @@ import { apiUrl } from 'helpers/config';
 
 const questionService = {
     rateQuestion,
-    fetchQuestion
+    fetchQuestion,
+    listQuestions
 };
-
-
-export const handleResponse = (response) => {
-    return response.json().then(data => {
-        if (!response.ok) {
-            const error = (data && data.email);
-            return Promise.reject(error);
-        }
-
-        return data;
-    });
-}
 
 function fetchQuestion(id) {
     const requestOptions = {
@@ -23,10 +12,45 @@ function fetchQuestion(id) {
         headers: { 'Content-Type': 'application/json' }
     };
 
+    const handleResponse = (response) => {
+        return response.json().then(data => {
+            if (!response.ok) {
+                const error = (data && data.email);
+                return Promise.reject(error);
+            }
+    
+            return data;
+        });
+    }
+
     return fetch(`${apiUrl}/questions/${id}/`, requestOptions)
         .then(handleResponse)
         .then(activeQuestion => {
             return activeQuestion;
+        });
+}
+
+function listQuestions(page) {
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    };
+
+    const handleResponse = (response) => {
+        return response.json().then(data => {
+            if (!response.ok) {
+                const error = (data && data.email);
+                return Promise.reject(error);
+            }
+    
+            return data;
+        });
+    }
+
+    return fetch(`${apiUrl}/questions/?page=${page}`, requestOptions)
+        .then(handleResponse)
+        .then(questionPage => {
+            return questionPage;
         });
 }
 
