7987f76213b6f4cd8b3f00bc16a72a3270883403
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-07-12 23:14:37 -0300

    Add question page actions and respectives reducers

--- src/reducers/questionReducer.js
+++ src/reducers/questionReducer.js
@@ -1,6 +1,7 @@
 import {
     FETCH_QUESTION, FETCH_QUESTION_SUCCESS, FETCH_QUESTION_FAILURE,
-    RATE_QUESTION
+    RATE_QUESTION,
+    LIST_QUESTION_PAGE, LIST_QUESTION_PAGE_SUCCESS, LIST_QUESTION_PAGE_FAILURE
 } from 'actions/questionAction';
 
 const initialState = {
@@ -28,11 +29,32 @@ export const question = (state = initialState, action) => {
                 error: action.error
             })
         break;
+
+        case LIST_QUESTION_PAGE:
+            return Object.assign({}, state, {
+                isFetching : true,
+                error: null
+            });
+        break;
+        case LIST_QUESTION_PAGE_SUCCESS:
+            return Object.assign({}, state, {
+                questionPage: action.questionPage,
+                isFetching: false
+            });
+        break;
+        case LIST_QUESTION_PAGE_FAILURE:
+            return Object.assign({}, state, {
+                isFetching: false,
+                error: action.error
+            })
+        break;
+
         case RATE_QUESTION:
             return Object.assign({}, state, {
                 rating: action.rating
         })
         break;
+
         default:
             return state;
     }
