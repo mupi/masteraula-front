cb361c0456d8d818ec2c64a7aa11c5ac51c6f551
pamelars86 <pamela.rosales@pucp.pe>
2018-07-16 16:15:35 -0300

    message Loading question per page

--- src/pages/QuestionBase/QuestionBasePage.js
+++ src/pages/QuestionBase/QuestionBasePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Row, Input, InputGroup, InputGroupAddon, Button} from 'reactstrap';
+import { Row, Input, InputGroup, InputGroupAddon, Button, Alert} from 'reactstrap';
 import HomeUserPage from "../HomeUser/HomeUserPage"
 import QuestionList from "components/question/QuestionList"
 import  QuestionPagination from "components/QuestionPagination/QuestionPagination"
 import 'assets/css/QuestionBase.css';
+
+
+const getResults = (isFetching, results ) => {
+  if(!isFetching) {
+        return <QuestionList questions={results} numCols='3'/>
+      }
+      else{
+        return <Alert color="warning">Carregando ...</Alert>
+      }
+}
 
 class QuestionBasePage extends React.Component {
     componentDidMount() {
@@ -16,10 +26,10 @@ class QuestionBasePage extends React.Component {
         this.props.listQuestions(parseInt(this.props.match.params.page));
       }
     }
-    
+
     render(){
-      const { questionPage, error } = this.props
-  
+      const { questionPage,isFetching, error } = this.props
+
       if(error) {
         return  (
           <HomeUserPage>
@@ -27,7 +37,8 @@ class QuestionBasePage extends React.Component {
           </HomeUserPage>
         )
       }
-      
+
+
       return (
         <HomeUserPage showFilters={true}>
           <div className="contenedor-question-base">
@@ -41,7 +52,7 @@ class QuestionBasePage extends React.Component {
                 <QuestionPagination {...this.props} {...questionPage}/>
               </Row>
               <Row className="questions-result">
-                <QuestionList questions={questionPage.results} numCols='3'/>
+                {getResults(isFetching,questionPage.results)}
               </Row>
               <Row className="questions-result">
                 <QuestionPagination {...this.props} {...questionPage}/>
