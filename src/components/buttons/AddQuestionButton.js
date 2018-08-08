d2963f3525b500d1abb98cf45187035a09e72953
pamelars86 <pamela.rosales@pucp.pe>
2018-08-08 17:08:56 -0300

    AddQuestionButton will have a single method to call createDocument when document doesn't exist yet

--- src/components/buttons/AddQuestionButton.js
+++ src/components/buttons/AddQuestionButton.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 
-const AddQuestionButton = ({ customClass, questionId, nameButton, onClick }) => (
+const handleAddQuestionButton = (e,questionId) => {
+  console.log("hola");
+  console.log(questionId);
+//  addQuestion(id);
+};
+
+
+
+const AddQuestionButton = ({ customClass, questionId, nameButton }) => (
   <Button
     value={questionId}
     title="Adicionar questões"
     className={customClass}
-    onClick={onClick}
+    onClick={e => handleAddQuestionButton(e, questionId)}
   >
     <i className="fa fa-plus" />
     {' '}
--- src/components/question/QuestionCard.js
+++ src/components/question/QuestionCard.js
@@ -13,60 +13,53 @@ import QuestionSourceYear from './QuestionSourceYear';
 
 const QuestionCard = ({
 id, disciplines, source, year, statement, urlImage = '', author, teaching_levels, addQuestion, removeQuestion}) => {
-  const handleAddQuestionButton = (e) => {
-    console.log(id);
-  //  addQuestion(id);
-  };
 
+  return (
+    <Card className={urlImage !== '' ? 'h-10 image-card' : 'h-100'}>
+      { urlImage !== '' ? <CardImg className="question-card__image" top width="100%" src={imageCard} alt="Card image cap" /> : null }
+      <CardBody className="question-card__body">
+        <Row>
+          <DisciplineList list={disciplines} />
+        </Row>
+        <Row>
+          <QuestionSourceYear source={source} year={year} />
+        </Row>
+        { !urlImage
+          ? (
+            <Row>
+              <TagList list={teaching_levels} styleTag="question-info teaching-level" />
+            </Row>
+          ) : null}
 
-  return(
-  <Card className={urlImage !== '' ? 'h-10 image-card' : 'h-100'}>
-    { urlImage !== '' ? <CardImg className="question-card__image" top width="100%" src={imageCard} alt="Card image cap" /> : null }
-    <CardBody className="question-card__body">
-      <Row>
-        <DisciplineList list={disciplines} />
-      </Row>
-      <Row>
-        <QuestionSourceYear source={source} year={year} />
-      </Row>
-      { !urlImage
-        ? (
-          <Row>
-            <TagList list={teaching_levels} styleTag="question-info teaching-level" />
-          </Row>
-        ) : null
-			}
-
-      <div className="l-question-card-text">
-        <p className="question-info__more-info">
+        <div className="l-question-card-text">
+          <p className="question-info__more-info">
           Autor:
-          <QuestionAuthor author={author} styleTag="question-info__author" />
-        </p>
-        <p className="question-info__more-info">
-          {' '}
-          { statement.substring(0, 150) }
-          {' '}
-          {statement.length >= 150 && (
-          <span>
-...
-          </span>
-          )}
-        </p>
-      </div>
-      <Link to={`/view-question/${id}`}>
-        <Button className="question-card__btn">
-          Ver mais
-        </Button>
-      </Link>
+            <QuestionAuthor author={author} styleTag="question-info__author" />
+          </p>
+          <p className="question-info__more-info">
+            {' '}
+            { statement.substring(0, 150) }
+            {' '}
+            {statement.length >= 150 && (
+              <span>
+                ...
+              </span>
+            )}
+          </p>
+        </div>
+        <Link to={`/view-question/${id}`}>
+          <Button className="question-card__btn">
+            Ver mais
+          </Button>
+        </Link>
 
-      <AddQuestionButton
-        questionId={id}
-        customClass="question-card__btn"
-        nameButton="Adicionar"
-        onClick={e => handleAddQuestionButton(e)}
-      />
-    </CardBody>
-  </Card>
+        <AddQuestionButton
+          questionId={id}
+          customClass="question-card__btn"
+          nameButton="Adicionar"
+        />
+      </CardBody>
+    </Card>
   );
 };
 export default QuestionCard;
