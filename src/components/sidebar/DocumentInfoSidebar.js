6c4c7b2af20e03ec094f366b7116e42900e9a803
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-08-15 12:15:26 -0300

    Fix button Editar not working and eslint

--- src/components/sidebar/DocumentInfoSidebar.js
+++ src/components/sidebar/DocumentInfoSidebar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 import ExportDocumentButton from 'components/buttons/ExportDocumentButton';
 
-const DocumentInfoSidebar = ({ documentName="Sem nome", documentTotalQuestions=0}) => (
+const DocumentInfoSidebar = ({ documentName, documentTotalQuestions }) => (
   <div className="c-sidebar__document-info">
     <h5>
       Documento Atual
@@ -12,22 +13,29 @@ const DocumentInfoSidebar = ({ documentName="Sem nome", documentTotalQuestions=0
     <h6>
       <i className="fa fa-file btn__icon" />
       <Link className="c_sidebar__document-link" to="/edit-document">
-        {documentName} {' '}
-        (
-        {documentTotalQuestions}
-        )
-        </Link>
+        {`${documentName} (${documentTotalQuestions})`}
+      </Link>
     </h6>
     <div className="c-sidebar__document-l-buttons">
-      <Button className="btn-margin-right">
-        <Link className="c-sidebar__document-btn-item" to="/edit-document">
+      <Link className="c-sidebar__document-btn-item" to="/edit-document">
+        <Button className="btn-margin-right">
           <i className="fa fa-pencil btn__icon" />
           Editar
-        </Link>
-      </Button>
+        </Button>
+      </Link>
       <ExportDocumentButton />
     </div>
   </div>
-
 );
+
+DocumentInfoSidebar.propTypes = {
+  documentName: PropTypes.string,
+  documentTotalQuestions: PropTypes.number,
+};
+
+DocumentInfoSidebar.defaultProps = {
+  documentName: 'Sem nome',
+  documentTotalQuestions: 0,
+};
+
 export default DocumentInfoSidebar;
