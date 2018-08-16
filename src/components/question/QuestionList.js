f340b0c93dc46678e05536a792eccf09c2366da0
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-08-16 14:34:37 -0300

    Add propTypes to QuestionList Fix some eslint errors and warnings

--- src/components/question/QuestionList.js
+++ src/components/question/QuestionList.js
@@ -1,28 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Row, Col } from 'reactstrap';
 import QuestionCard from './QuestionCard';
 
-const QuestionList = ({
-  questions = [], count=0, sm = 4,  toggleModal, modal, activeDocument, addQuestion, addSelectedQuestion
-}) => (
+const QuestionList = ({ questions, count, sm }) => (
   <Row>
     <Col sm="12" className="c-question-base__total-results">
-      Questões encontradas:
-      { count }
+      {`Questões encontradas: ${count}`}
     </Col>
-    {questions.map((question, i) => (
-      <Col sm={sm} xs="12" key={i} className="question-card">
-        <QuestionCard
-          {...question}
-          toggleModal={toggleModal}
-          modal={modal}
-          activeDocument={activeDocument}
-          addQuestion={addQuestion}
-          addSelectedQuestion={addSelectedQuestion}
-        />
+    {questions.map(question => (
+      <Col sm={sm} xs="12" className="question-card">
+        <QuestionCard {...question} {...this.props} />
       </Col>
     ))}
   </Row>
 );
+
+QuestionList.propTypes = {
+  questions: PropTypes.arrayOf(PropTypes.shape({})),
+  count: PropTypes.number,
+  sm: PropTypes.number,
+};
+
+QuestionList.defaultProps = {
+  questions: [],
+  count: 0,
+  sm: 4,
+};
 
 export default QuestionList;
