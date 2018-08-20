85a19111490387b12f3812547758bc30c469fed3
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-08-20 11:41:30 -0300

    Fix error with toggle, fix not working Ver Mais button

--- src/components/document/DocumentQuestions.js
+++ src/components/document/DocumentQuestions.js
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {
-  Row, Container, Col, Label, Button, Modal, ModalBody, ModalFooter, ModalHeader,
+  Row, Container, Col, Button,
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import QuestionContent from 'components/question/QuestionContent';
 import ViewQuestionModal from 'components/question/ViewQuestionModal';
 import DisciplineList from 'components/disciplines/DisciplineList';
@@ -11,93 +12,57 @@ import QuestionSourceYear from 'components/question/QuestionSourceYear';
 import GoToQuestionBaseButton from 'components/buttons/GoToQuestionBaseButton';
 import RemoveQuestionButton from 'components/buttons/RemoveQuestionButton';
 
-const getListQuestions = (activeDocument, removeSelectedQuestion) => {
-  const questions = [];
-  if (activeDocument) {
-    for (let i = 0; i < activeDocument.questions.length; i += 1) {
-      const question = activeDocument.questions[i];
-      questions.push(
-        <div key={i} className="c-document__question">
-          <RemoveQuestionButton
-            questionId={question.question}
-            activeDocumentId={activeDocument.id}
-            removeSelectedQuestion={removeSelectedQuestion}
-          />
-          <Row>
-            <Col sm="12">
-              <DisciplineList list={question.disciplines} />
-              <QuestionSourceYear source={question.source} year={question.year} />
-            </Col>
-          </Row>
-          <Row>
-            <div className="c-document__question-content">
-              <QuestionContent statement={question.question} />
-            </div>
-          </Row>
-          <Row>
-            <div className="c-document__question-view-more col-md-3 offset-md-9">
-              <Button onClick={() => this.toggle(question)}>
-                <i className="fa fa-search" />
-                {' '}
-                <span className="button-text">
-                  Ver mais
-                </span>
-              </Button>
-            </div>
-          </Row>
-        </div>,
-      );
-    }
-  }
-  return questions;
+const DocumentQuestions = (props) => {
+  const { activeDocument, removeSelectedQuestion } = props;
+
+  return (
+    <Container>
+      <div>
+        <div className="l-button-add-question">
+          <GoToQuestionBaseButton customClass="o-button-add-question-doc o-button-add-question-doc--xl" />
+        </div>
+        {activeDocument && activeDocument.questions.map(question => (
+          <div className="c-document__question">
+            <RemoveQuestionButton
+              questionId={question.question}
+              activeDocumentId={activeDocument.id}
+              removeSelectedQuestion={removeSelectedQuestion}
+            />
+            <Row>
+              <Col sm="12">
+                <DisciplineList list={question.disciplines} />
+                <QuestionSourceYear source={question.source} year={question.year} />
+              </Col>
+            </Row>
+            <Row>
+              <div className="c-document__question-content">
+                <QuestionContent statement={question.question} />
+              </div>
+            </Row>
+            <Row>
+              <div className="c-document__question-view-more col-md-3 offset-md-9">
+                <Link to={`/view-question/${question.id}`}>
+                  <Button className="question-card__btn">
+                    Ver mais
+                  </Button>
+                </Link>
+              </div>
+            </Row>
+          </div>
+        ))}
+        <ViewQuestionModal />
+      </div>
+
+    </Container>);
 };
 
-class DocumentQuestions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      question: '',
-    };
-
-    this.toggle = this.toggle.bind(this);
-  }
-
-   toggle(question) {
-    if (question) {
-      this.setState({
-        question,
-      });
-    } else {
-      this.setState({
-        question: '',
-      });
-    }
-  }
-
-
-  render() {
-    const { activeDocument, removeSelectedQuestion } = this.props;
-    const questions = getListQuestions(activeDocument, removeSelectedQuestion);
-
-    return (
-      <Container>
-        <div>
-          <div className="l-button-add-question">
-            <GoToQuestionBaseButton customClass="o-button-add-question-doc o-button-add-question-doc--xl" />
-          </div>
-          {questions}
-          <ViewQuestionModal />
-        </div>
-
-      </Container>);
-  }
-}
-
 DocumentQuestions.propTypes = {
+  activeDocument: PropTypes.shape(),
   removeSelectedQuestion: PropTypes.func,
 };
 
 DocumentQuestions.defaultProps = {
+  activeDocument: null,
   removeSelectedQuestion: f => f,
 };
 
