8fcd52a77ca9832b58e5d5dc7ca0151a5c78273f
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-08-20 09:56:09 -0300

    Call remove question document action from loginAction, not session

--- src/actions/sessionAction.js
+++ src/actions/sessionAction.js
@@ -9,7 +9,6 @@ export const updateSession = (session) => {
 
 export const deleteSession = (session) => {
   localStorage.removeItem('session');
-  localStorage.removeItem('activeDocument');
   return { type: DELETE_SESSION, session };
 };
 
