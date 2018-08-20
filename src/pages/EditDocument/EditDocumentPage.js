a40e3d84e95652580086dff6bdc75a727ee99199
Daniela Palumbo <d166301@dac.unicamp.br>
2018-08-20 18:40:41 -0300

    Error/success messages when editing document

--- src/pages/EditDocument/EditDocumentPage.js
+++ src/pages/EditDocument/EditDocumentPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+ import { Alert } from 'reactstrap';
 import DocumentHeader from 'components/document/DocumentHeader';
 import DocumentQuestions from 'components/document/DocumentQuestions';
 import HomeUserPage from 'pages/HomeUser/HomeUserPage';
@@ -8,11 +8,30 @@ import HomeUserPage from 'pages/HomeUser/HomeUserPage';
 class EditDocumentPage extends Component {
 
   render() {
-    const { activeDocument, removeSelectedQuestion, submit } = this.props;
+    const { activeDocument, removeSelectedQuestion, submit, isUpdated, error, isRemoved } = this.props;
+    
 
     return (
       <HomeUserPage>
         <div className="c-document">
+        {isUpdated ? (  
+              <Alert className="alert--success" color="success">
+                  Documento editado com sucesso
+              </Alert>
+          ):''
+        }
+        {error ? (  
+              <Alert className="alert--danger" color="danger">
+                  Ocorreu algum erro com sua solicitação, tente novamente.
+              </Alert>
+          ):''
+        }
+        {isRemoved ? (  
+              <Alert className="alert--success" color="success">
+                  Questão removida com sucesso
+              </Alert>
+          ):''
+        }
           <DocumentHeader data={activeDocument} onSubmit={submit}/>
           <DocumentQuestions
             activeDocument={activeDocument}
