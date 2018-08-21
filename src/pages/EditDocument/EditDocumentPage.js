a8a20f19a308eac99d539ad93de838f66b0c5aa5
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-08-21 17:12:11 -0300

    Prevent erro if activeDocument is null and edit-document is accessed

--- src/pages/EditDocument/EditDocumentPage.js
+++ src/pages/EditDocument/EditDocumentPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { history } from 'helpers/history';
 
 import DocumentHeader from 'components/document/DocumentHeader';
 import DocumentQuestions from 'components/document/DocumentQuestions';
@@ -9,7 +10,11 @@ class EditDocumentPage extends Component {
 
   componentDidMount() {
     const { fetchDocument, activeDocument } = this.props;
-    fetchDocument(parseInt(activeDocument.id, 10));
+    if (!activeDocument) {
+      history.push('/documents/1');
+    } else {
+      fetchDocument(parseInt(activeDocument.id, 10));
+    }
   }
 
   render() {
@@ -35,7 +40,6 @@ EditDocumentPage.propTypes = {
 };
 
 EditDocumentPage.defaultProps = {
-  activeDocument: null,
   removeSelectedQuestion: f => f,
   submit: f => f,
 };
