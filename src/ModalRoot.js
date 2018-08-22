4abfc35cf912c951407f1263b06d0e331e465402
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-08-22 12:09:01 -0300

    Fix some eslint warnings

--- src/ModalRoot.js
+++ src/ModalRoot.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 import ReactModal from 'react-modal';
 
-import { default as modalTypes } from 'components/modal';
+import modalTypes from 'components/modal';
 
 const MODAL_TYPES = {
-  'alert': modalTypes.alertModal,
-  'confirm': modalTypes.confirmModal,
-  'delete': modalTypes.deleteModal,
-  'prompt': modalTypes.promptModal,
-  'document': modalTypes.documentModal,
-}
+  alert: modalTypes.alertModal,
+  confirm: modalTypes.confirmModal,
+  delete: modalTypes.deleteModal,
+  prompt: modalTypes.promptModal,
+  document: modalTypes.documentModal,
+};
 
 const mapStateToProps = state => ({
   ...state.modal,
@@ -22,30 +22,33 @@ class ModalContainer extends React.Component {
     this.state = {
       modalIsOpen: false,
     };
-    this.closeModal = this.closeModal.bind(this)
+    this.closeModal = this.closeModal.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps !== this.props) {
       this.setState({
-        modalIsOpen: nextProps.modalProps.open
-      })
+        modalIsOpen: nextProps.modalProps.open,
+      });
     }
   }
 
   closeModal() {
-    this.setState({ modalIsOpen: false })
+    this.setState({ modalIsOpen: false });
   }
 
   render() {
-    if (!this.props.modalType) {
-      return null
+    const { modalType, modalProps } = this.props;
+    const { modalIsOpen } = this.state;
+
+    if (!modalType) {
+      return null;
     }
-    const SpecifiedModal = MODAL_TYPES[this.props.modalType]
+    const SpecifiedModal = MODAL_TYPES[modalType];
     return (
       <div>
         <ReactModal
-          isOpen={this.state.modalIsOpen}
+          isOpen={modalIsOpen}
           onRequestClose={this.closeModal}
           contentLabel="Example Modal"
           ariaHideApp={false}
@@ -56,12 +59,12 @@ class ModalContainer extends React.Component {
         >
           <SpecifiedModal
             closeModal={this.closeModal}
-            {...this.props.modalProps}
+            {...modalProps}
           />
         </ReactModal>
       </div>
-    )
+    );
   }
 }
 
-export default connect(mapStateToProps, null)(ModalContainer)
+export default connect(mapStateToProps, null)(ModalContainer);
