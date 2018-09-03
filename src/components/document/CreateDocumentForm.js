823a2a079568aa071665eb23830f984fc2ddc5a6
pamelars86 <pamela.rosales@pucp.pe>
2018-09-03 12:51:07 -0300

    eslint fixes

--- src/components/document/CreateDocumentForm.js
+++ src/components/document/CreateDocumentForm.js
@@ -4,7 +4,7 @@ import {
 } from 'reactstrap';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
-import { toggleModal, createDocument } from 'actions/documentAction';
+import { toggleModal } from 'actions/documentAction';
 
 const renderField = ({
   input,
@@ -39,7 +39,7 @@ const CreateDocumentForm = (props) => {
   const {
     handleSubmit, error, modal, toggleModal, messageWhenDocumentExist
   } = props;
-
+ 
   return(
     <div>
       {messageWhenDocumentExist ? messageWhenDocumentExist :  <p className="text-center">Por favor, insira um nome para o novo documento a ser criado</p>}
