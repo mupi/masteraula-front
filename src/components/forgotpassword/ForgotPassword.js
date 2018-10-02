100691e81876a74d322cd6b3aaad75dd08b2146a
pamelars86 <pamela.rosales@pucp.pe>
2018-10-02 13:01:25 -0300

    toogleModal in loginAction was updated to accept deleteSession when modal is opened

--- src/components/forgotpassword/ForgotPassword.js
+++ src/components/forgotpassword/ForgotPassword.js
@@ -40,12 +40,16 @@ const ForgotPassword = (props) => {
       <h3>
         Redefinição de senha 
       </h3>
-      <p>
+      { !success
+              &&  (<p>
         Informe abaixo o endereço de email que você cadastrou para receber a mensagem de redefinição de senha
-      </p>
+      </p>)
+          }
       <Row className="justify-content-center">
         <Col sm="12" xs="12">
           <Form onSubmit={handleSubmit}>
+          { !success
+              &&  (
             <FormGroup className="text-left">
               <Field
                 component={renderField}
@@ -56,18 +60,24 @@ const ForgotPassword = (props) => {
                 className="form-control"
               /> 
             </FormGroup>
+            )
+          }
             { success
               && (
               <Alert color="success">
                 <p className="alert__message">
-                  Caso este e-mail estiver cadastrado, enviamos um email com instruções para recuperar sua senha
+                  Caso o e-mail estiver cadastrado, enviamos um email com instruções para recuperar sua senha
                 </p>
               </Alert>
               )
             }
+
+            { !success
+              &&  (
             <Button>
               Enviar email
-            </Button>
+            </Button>)
+          }
           </Form>
         </Col>
       </Row>
