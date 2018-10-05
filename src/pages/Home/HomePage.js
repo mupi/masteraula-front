ba80b7ac416392e80ed5ace6f16ebe02c56e5297
pamelars86 <pamela.rosales@pucp.pe>
2018-10-05 14:37:22 -0300

    home was updated as original

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -10,29 +10,29 @@ const HomePage = () => (
       <Row>
         <Col md="12">
           <h1 className="c-public-home__main-title">
-Masteraula
+            Biblioteca inteligente de questões
           </h1>
           <Col md="6" className="offset-md-3">
             <p className="c-public-home__title-description">
 
-                Prepare suas provas em poucos minutos
+                MasterAula é uma ferramenta feita por educadores para educadores onde você encontra milhares de questões para montar provas e testes.
             </p>
           </Col>
         </Col>
       </Row>
     </div>
 
-    <Container className="c-public-home__features hidden">
+    <Container className="c-public-home__features">
       <Row>
         <Col md="4" className="text-center">
           <div className="o-thumb-circle">
             <img className="o-thumb-circle__img" src={moneyImg} alt="venda-questoes" />
           </div>
           <h4>
-Venda suas questões
+            Venda suas questões
           </h4>
           <p>
-Ganhe uma renda extra vendendo os materiais que você já criou para outros educadores da comunidade.
+            Ganhe uma renda extra vendendo os materiais que você já criou para outros educadores da comunidade.
           </p>
         </Col>
 
@@ -41,7 +41,7 @@ Ganhe uma renda extra vendendo os materiais que você já criou para outros educ
             <img className="o-thumb-circle__img" src={clockImg} alt="monte-questoes" />
           </div>
           <h4>
-Monte provas e testes em 5 minutos!
+            Monte provas e testes em 5 minutos!
           </h4>
           <p>
 Otimize seu tempo de preparar aulas encontrando materiais curados por educadores de todo o Brasil.
