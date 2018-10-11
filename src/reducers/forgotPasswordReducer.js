85fdbebefde42cc18bbb0996ede3733d60f7d84a
pamelars86 <pamela.rosales@pucp.pe>
2018-10-11 10:35:23 -0300

    toast were removed from forgotPasswordReducer.js because this is used when user isnt logged

--- src/reducers/forgotPasswordReducer.js
+++ src/reducers/forgotPasswordReducer.js
@@ -2,24 +2,12 @@ import {
   FORGOT_PASSWORD_RESET, FORGOT_PASSWORD_REQUEST, FORGOT_PASSWORD_SUCCESS, FORGOT_PASSWORD_FAILURE,
   CHANGE_PASSWORD_REQUEST, CHANGE_PASSWORD_SUCCESS, CHANGE_PASSWORD_FAILURE, CHANGE_PASSWORD_RESET,
 } from 'actions/forgotPasswordAction';
-import { toast } from 'react-toastify';
 
 const initialState = {
   success: false,
   isFetching: false,
   error: null,
 };
-
-const optionsSuccess = {
-  className: 'alert__ma-toast--success',
-  type: 'success',
-};
-
-const optionsError = {
-  className: 'alert__ma-toast--error',
-  type: 'error',
-};
-
 
 export function forgotPassword(state = initialState, action) {
   switch (action.type) {
@@ -53,14 +41,12 @@ export function forgotPassword(state = initialState, action) {
         error: null,
       });
     case CHANGE_PASSWORD_SUCCESS: {
-      toast.success('Senha alterada com sucesso', optionsSuccess);
       return Object.assign({}, state, {
         isFetching: false,
         success: true,
       });
     }
     case CHANGE_PASSWORD_FAILURE:
-      toast.error('Ocorreu um erro com sua solicitação', optionsError);
       return Object.assign({}, state, {
         isFetching: false,
         error: action.error,
