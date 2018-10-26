bdf38fba8ada01b9120210fdc7e0ab088f315f08
pamelars86 <pamela.rosales@pucp.pe>
2018-10-26 14:57:09 -0200

    final styles in top menu with document options

--- src/components/sidebar/DocumentInfoSidebar.js
+++ src/components/sidebar/DocumentInfoSidebar.js
@@ -1,55 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button , UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
+import { Button , UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 import ExportDocumentButtonContainer from 'containers/ExportDocumentButtonContainer';
- 
+
 const DocumentInfoSidebar = ({ documentName, documentTotalQuestions, documentId }) => (
-  <div className="c-sidebar__document-info">
-    <div className="c-sidebar__document-name-section">
-      <small className="c-sidebar__document-message">Você está editando: </small>
-      <Link className="c-sidebar__document-link" to="/edit-document">
-        {`${documentName}`}  
+  <div className="menu-top__document-info">
+    <div className="menu-top__document-name-section">
+      <small className="menu-top__document-message">Você está editando: </small>
+      <Link className="menu-top__document-link" to="/edit-document">
+        {`${documentName}`}
       </Link>
     </div>
-    <div className="c-sidebar__document-l-buttons">
-    <a>
-      <p className="c-sidebar__document-questions  btn__icon">
-      <small className="c-sidebar__document-message">Questões: {documentTotalQuestions} </small></p>
-
+    <div className="menu-top__document-l-buttons">
+      <a>
+        <p className="menu-top__document-questions  btn__icon">
+          <small className="menu-top__document-message">
+            Questões:
+            {' '}<span className="menu-top__document-number-questions"><strong>{documentTotalQuestions}</strong></span>
+          </small>
+        </p>
       </a>
-      <Link className="c-sidebar__document-btn-item" to="/edit-document">
-        <Button className="btn-margin-right c-sidebar__document-button">
+      <Link className="menu-top__document-btn-item" to="/edit-document">
+        <Button className="btn-margin-right menu-top__document-button">
           <i className="fa fa-pencil btn__icon" />
           Editar
         </Button>
       </Link>
       <a>
-      <ExportDocumentButtonContainer
-        text="Exportar"
-        styleCustomize="c-sidebar__document-button"
-        documentId={documentId}
-        documentName={documentName}
-        documentTotalQuestions={documentTotalQuestions}
-      />
+        <ExportDocumentButtonContainer
+          text="Exportar"
+          styleCustomize="menu-top__document-button"
+          documentId={documentId}
+          documentName={documentName}
+          documentTotalQuestions={documentTotalQuestions}
+        />
       </a>
       <a>
-      <UncontrolledDropdown className="">
-                      <DropdownToggle className="c-sidebar__document-button" caret size="sm">
-                        Trocar prova
-                      </DropdownToggle>
-                      <DropdownMenu>
-                        <DropdownItem>
-                          Prova 1
-                        </DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem>
-                          Mais provas
-                        </DropdownItem>
-                      </DropdownMenu>
-          </UncontrolledDropdown></a>
-    
+        <UncontrolledDropdown className="">
+          <DropdownToggle className="menu-top__document-button" caret size="sm">
+            Trocar prova
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>
+              Prova 1
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              Mais provas
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </a>
     </div>
   </div>
 );
