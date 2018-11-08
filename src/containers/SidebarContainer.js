0fe835adf5a31a665a0127589d5535cd7873858c
pamelars86 <pamela.rosales@pucp.pe>
2018-11-08 12:32:36 -0200

    working on dropdown when user logout

--- src/containers/SidebarContainer.js
+++ src/containers/SidebarContainer.js
@@ -1,19 +1,21 @@
 import { connect } from 'react-redux';
 import Sidebar from 'components/sidebar/Sidebar';
 import { logout } from 'actions/loginAction';
-import { openSidebar } from 'actions/menuAction';
+import { toggleMenu, openSidebar } from 'actions/menuAction';
 
 
 // state.<reducer's name>.<property>
 const mapStateToProps = state => ({
+  isOpen: state.menu.isOpen,
   user: state.session.session.user,
   activeDocument: state.document.activeDocument,
   isOpenSidebar: state.menu.isOpenSidebar,
 });
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout()),
+  toggleMenu: isOpen => dispatch(toggleMenu(isOpen)),
   openSidebar: isOpenSidebar => dispatch(openSidebar(isOpenSidebar)),
+  logout: () => dispatch(logout()),
 });
 
 export default connect(
