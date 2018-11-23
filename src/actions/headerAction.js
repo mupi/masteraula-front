aaa3ae47d135379e241e4aa2cd6cfa0ecc19a96f
pamelars86 <pamela.rosales@pucp.pe>
2018-11-23 14:56:27 -0200

    headerAction.js contains all actions available for a header

--- src/actions/headerAction.js
+++ src/actions/headerAction.js
@@ -1,266 +1,127 @@
-import { documentService } from 'services';
+import { headerService } from 'services';
 import { history } from 'helpers/history';
-import FileSaver from 'file-saver';
 
-// Load single document
-export const FETCH_DOCUMENT = 'FETCH_DOCUMENT';
-export const FETCH_DOCUMENT_SUCCESS = 'FETCH_DOCUMENT_SUCCESS';
-export const FETCH_DOCUMENT_FAILURE = 'FETCH_DOCUMENT_FAILURE';
+// Fetch header
+export const FETCH_HEADER = 'FETCH_HEADER';
+export const FETCH_HEADER_SUCCESS = 'FETCH_HEADER_SUCCESS';
+export const FETCH_HEADER_FAILURE = 'FETCH_HEADER_FAILURE';
 
-// Load preview document
-export const FETCH_PREVIEW_DOCUMENT = 'FETCH_PREVIEW_DOCUMENT';
-export const FETCH_PREVIEW_DOCUMENT_SUCCESS = 'FETCH_PREVIEW_DOCUMENT_SUCCESS';
-export const FETCH_PREVIEW_DOCUMENT_FAILURE = 'FETCH_PREVIEW_DOCUMENT_FAILURE';
+// Create new header
+export const CREATE_HEADER = 'CREATE_HEADER';
+export const CREATE_HEADER_SUCCESS = 'CREATE_HEADER_SUCCESS';
+export const CREATE_HEADER_FAILURE = 'CREATE_HEADER_FAILURE';
+export const RESET_NEW_HEADER = 'RESET_NEW_HEADER';
 
-// Create new document
-export const CREATE_DOCUMENT = 'CREATE_DOCUMENT';
-export const CREATE_DOCUMENT_SUCCESS = 'CREATE_DOCUMENT_SUCCESS';
-export const CREATE_DOCUMENT_FAILURE = 'CREATE_DOCUMENT_FAILURE';
-export const RESET_NEW_DOCUMENT = 'RESET_NEW_DOCUMENT';
+// Update header
+export const UPDATE_HEADER = 'UPDATE_HEADER';
+export const UPDATE_HEADER_SUCCESS = 'UPDATE_HEADER_SUCCESS';
+export const UPDATE_HEADER_FAILURE = 'UPDATE_HEADER_FAILURE';
+export const RESET_UPDATE_HEADER = 'RESET_UPDATE_HEADER';
 
-// Update document header
-export const UPDATE_DOCUMENT = 'UPDATE_DOCUMENT';
-export const UPDATE_DOCUMENT_SUCCESS = 'UPDATE_DOCUMENT_SUCCESS';
-export const UPDATE_DOCUMENT_FAILURE = 'UPDATE_DOCUMENT_FAILURE';
-export const RESET_UPDATE_DOCUMENT = 'RESET_UPDATE_DOCUMENT';
+// Delete header
+export const DELETE_HEADER = 'DELETE_HEADER';
+export const DELETE_HEADER_SUCCESS = 'DELETE_HEADER_SUCCESS';
+export const DELETE_HEADER_FAILURE = 'DELETE_HEADER_FAILURE';
+export const RESET_DELETE_HEADER = 'RESET_DELETE_HEADER';
 
-export const DELETE_DOCUMENT = 'DELETE_DOCUMENT';
-export const DELETE_DOCUMENT_SUCCESS = 'DELETE_DOCUMENT_SUCCESS';
-export const DELETE_DOCUMENT_FAILURE = 'DELETE_DOCUMENT_FAILURE';
-export const RESET_DELETE_DOCUMENT = 'RESET_DELETE_DOCUMENT';
-
-// List my Documents
-export const LIST_MY_DOCUMENTS = 'LIST_MY_DOCUMENTS';
-export const LIST_MY_DOCUMENTS_SUCCESS = 'LIST_MY_DOCUMENTS_SUCCESS';
-export const LIST_MY_DOCUMENTS_FAILURE = 'LIST_MY_DOCUMENTS_FAILURE';
+// List my headers
+export const LIST_MY_HEADERS = 'LIST_MY_HEADERS';
+export const LIST_MY_HEADERS_SUCCESS = 'LIST_MY_HEADERS_SUCCESS';
+export const LIST_MY_HEADERS_FAILURE = 'LIST_MY_HEADERS_FAILURE';
 
 
-export const fetchDocument = (id) => {
-  function requestDocument() { return { type: FETCH_DOCUMENT }; }
-  function fetchDocumentSuccess(activeDocument) { return { type: FETCH_DOCUMENT_SUCCESS, activeDocument }; }
-  function fetchDocumentFailure(error) { return { type: FETCH_DOCUMENT_FAILURE, error }; }
+// Function 1: Fetch Header
+export const fetchHeader = (id) => {
+  function requestHeader() { return { type: FETCH_HEADER }; }
+  function fetchHeaderSuccess(activeHeader) { return { type: FETCH_HEADER_SUCCESS, activeHeader }; }
+  function fetchHeaderFailure(error) { return { type: FETCH_HEADER_FAILURE, error }; }
   return (dispatch) => {
-    dispatch(requestDocument(id));
-    return documentService.fetchDocument(id).then(
-      (activeDocument) => {
-        dispatch(fetchDocumentSuccess(activeDocument));
+    dispatch(requestHeader(id));
+    return headerService.fetchHeader(id).then(
+      (activeHeader) => {
+        dispatch(fetchHeaderSuccess(activeHeader));
       }, (error) => {
-        dispatch(fetchDocumentFailure(error));
+        dispatch(fetchHeaderFailure(error));
       },
     );
   };
 };
 
-export const fetchPreviewDocument = (id) => {
-  function requestDocument() { return { type: FETCH_PREVIEW_DOCUMENT }; }
-  function fetchDocumentSuccess(previewDocument) { return { type: FETCH_PREVIEW_DOCUMENT_SUCCESS, previewDocument }; }
-  function fetchDocumentFailure(error) { return { type: FETCH_PREVIEW_DOCUMENT_FAILURE, error }; }
-  return (dispatch, getState) => {
-    if (getState().document.isFetchingPreviewDocument) {
-      return 1;
-    }
-    dispatch(requestDocument(id));
-    return documentService.fetchDocument(id).then(
-      (previewDocument) => {
-        dispatch(fetchDocumentSuccess(previewDocument));
-      }, (error) => {
-        dispatch(fetchDocumentFailure(error));
-      },
-    );
-  };
-};
-
-// Create a new document
-export const createDocument = (props) => {
-  function createNewDocument() { return { type: CREATE_DOCUMENT }; }
-  function createDocumentSuccess(newDocument) { return { type: CREATE_DOCUMENT_SUCCESS, newDocument }; }
-  function createDocumentFailure(error) { return { type: CREATE_DOCUMENT_FAILURE, error }; }
+// Function 2: Create a new header
+export const createHeader = (props) => {
+  function createNewHeader() { return { type: CREATE_HEADER }; }
+  function createHeaderSuccess(newHeader) { return { type: CREATE_HEADER_SUCCESS, newHeader }; }
+  function createHeaderFailure(error) { return { type: CREATE_HEADER_FAILURE, error }; }
   return (dispatch) => {
-    dispatch(createNewDocument(props));
-    return documentService.createDocument(props).then(
-      (newDocument) => {
-        dispatch(createDocumentSuccess(newDocument));
+    dispatch(createNewHeader(props));
+    return headerService.createHeader(props).then(
+      (newHeader) => {
+        dispatch(createHeaderSuccess(newHeader));
         history.push('/question-base/1');
       },
       (error) => {
-        dispatch(createDocumentFailure(error));
+        dispatch(createHeaderFailure(error));
       },
     );
   };
 };
 
-// Update an active Document
-export const updateDocument = (props) => {
-  function updateActiveDocument() { return { type: UPDATE_DOCUMENT }; }
-  function updateDocumentSuccess(activeDocument) { return { type: UPDATE_DOCUMENT_SUCCESS, activeDocument }; }
-  function updateDocumentFailure(error) { return { type: UPDATE_DOCUMENT_FAILURE, error }; }
+// Function 3: Update an active header
+export const updateHeader = (props) => {
+  function updateActiveHeader() { return { type: UPDATE_HEADER }; }
+  function updateHeaderSuccess(activeHeader) { return { type: UPDATE_HEADER_SUCCESS, activeHeader }; }
+  function updateHeaderFailure(error) { return { type: UPDATE_HEADER_FAILURE, error }; }
   return (dispatch) => {
-    dispatch(updateActiveDocument(props));
-    return documentService.updateDocument(props).then(
-      (activeDocument) => {
-        dispatch(updateDocumentSuccess(activeDocument));
+    dispatch(updateActiveHeader(props));
+    return headerService.updateHeader(props).then(
+      (activeHeader) => {
+        dispatch(updateHeaderSuccess(activeHeader));
       },
       (error) => {
-        dispatch(updateDocumentFailure(error));
+        dispatch(updateHeaderFailure(error));
       },
     );
   };
 };
 
-export const listMyDocuments = (page, orderField, order) => (dispatch) => {
-  const success = myDocumentsList => (
-    dispatch({ type: LIST_MY_DOCUMENTS_SUCCESS, myDocumentsList }));
+// Function 4: Get all my headers as a list
+export const listMyHeaders = (page, orderField, order) => (dispatch) => {
+  const success = myHeadersList => (
+    dispatch({ type: LIST_MY_HEADERS_SUCCESS, myHeadersList }));
 
   const error = errorMessage => (
-    dispatch({ type: LIST_MY_DOCUMENTS_FAILURE, errorMessage }));
+    dispatch({ type: LIST_MY_HEADERS_FAILURE, errorMessage }));
 
   dispatch({
-    type: LIST_MY_DOCUMENTS, page, orderField, order,
+    type: LIST_MY_HEADERS, page, orderField, order,
   });
-  return documentService.listMyDocuments(page, orderField, order)
+  return headerService.listMyHeaders(page, orderField, order)
     .then(success)
     .catch(error);
 };
 
-// Shown in Top Menu - Only 5 documents
-export const listMyLastDocuments = (page, orderField, order) => (dispatch) => {
-  const success = myLastDocumentsList => (
-    dispatch({ type: LIST_MY_LAST_DOCUMENTS_SUCCESS, myLastDocumentsList }));
-
-  const error = errorMessage => (
-    dispatch({ type: LIST_MY_LAST_DOCUMENTS_FAILURE, errorMessage }));
-
-  dispatch({
-    type: LIST_MY_LAST_DOCUMENTS, page, orderField, order,
-  });
-  return documentService.listMyLastDocuments(page, orderField, order)
-    .then(success)
-    .catch(error);
-};
-
-// Add Selected Question to Document
-export const addSelectedQuestion = (idDocument, idQuestion, order) => {
-  function addQuestionToDocument() { return { type: ADD_SELECTED_QUESTION }; }
-  function addQuestionToDocumentSuccess(addedQuestion) { return { type: ADD_SELECTED_QUESTION_SUCCESS, addedQuestion }; }
-  function addQuestionToDocumentFailure(error) { return { type: ADD_SELECTED_QUESTION_FAILURE, error }; }
-
-  function listDocumentsAfterAddQuestionSuccess(addedQuestion) { return { type: LIST_DOCUMENTS_AFTER_ADDQUESTION_SUCCESS, addedQuestion }; }
-
-  return (dispatch, getState) => {
-    if (getState().document.isFetchingAddQuestion) {
-      return 1;
-    }
-    dispatch(addQuestionToDocument(idDocument, idQuestion, order));
-    return documentService.addSelectedQuestion(idDocument, idQuestion, order)
-      .then(
-        (addedQuestion) => {
-          dispatch(addQuestionToDocumentSuccess(addedQuestion));
-          dispatch(listDocumentsAfterAddQuestionSuccess(addedQuestion));
-        },
-        (error) => {
-          dispatch(addQuestionToDocumentFailure(error));
-        },
-      );
-  };
-};
-
-// Remove Selected Question from Document
-export const removeSelectedQuestion = (idDocument, idQuestion) => {
-  function removeQuestionFromDocument() { return { type: REMOVE_SELECTED_QUESTION }; }
-  function removeQuestionFromDocumentSuccess(idRemovedQuestion) { return { type: REMOVE_SELECTED_QUESTION_SUCCESS, idRemovedQuestion }; }
-  function removeQuestionFromDocumentFailure(error) { return { type: REMOVE_SELECTED_QUESTION_FAILURE, error }; }
-
-  function listDocumentsAfterRemoveQuestionSuccess(addedQuestion) { return { type: LIST_DOCUMENTS_AFTER_REMOVEQUESTION_SUCCESS, addedQuestion }; }
-
-  return (dispatch) => {
-    dispatch(removeQuestionFromDocument(idDocument, idQuestion));
-    return documentService.removeSelectedQuestion(idDocument, idQuestion)
-      .then(
-        (idRemovedQuestion) => {
-          dispatch(removeQuestionFromDocumentSuccess(idRemovedQuestion));
-          dispatch(listDocumentsAfterRemoveQuestionSuccess(idRemovedQuestion));
-        },
-        (error) => {
-          dispatch(removeQuestionFromDocumentFailure(error));
-        },
-      );
-  };
-};
-
-export function resetNewDocument() {
+// Function 5: Reset Header
+export function resetNewHeader() {
   return {
-    type: RESET_NEW_DOCUMENT,
+    type: RESET_NEW_HEADER,
   };
 }
 
-// Create Document Toggle Modal
-export const toggleModal = (modal, question) => ({
-  type: CREATE_DOCUMENT_TOGGLE_MODAL,
-  modal: !modal,
-  willAddQuestion: question || null,
-});
-
-// Switch active document
-export const switchActiveDocument = (doc, isRedirect = false) => {
-  function requestDocument() { return { type: FETCH_DOCUMENT }; }
-  function fetchDocumentSuccess(activeDocument) { return { type: SWITCH_ACTIVE_DOCUMENT, activeDocument }; }
-  function fetchDocumentFailure(error) { return { type: FETCH_DOCUMENT_FAILURE, error }; }
-
+// Function 6: Delete header
+export const deleteHeader = (idHeader) => {
+  function deleteSelectedHeader() { return { type: DELETE_HEADER }; }
+  function deleteSelectedHeaderSuccess(idHeaderRemoved) { return { type: DELETE_HEADER_SUCCESS, idHeaderRemoved }; }
+  function deleteSelectedHeaderFailure(error) { return { type: DELETE_HEADER_FAILURE, error }; }
   return (dispatch) => {
-    dispatch(requestDocument(doc.id));
-    return documentService.fetchDocument(doc.id).then(
-      (activeDocument) => {
-        dispatch(fetchDocumentSuccess(activeDocument));
-        localStorage.setItem('activeDocument', JSON.stringify(activeDocument));
-        if (isRedirect) history.push('/edit-document');
-      }, (error) => {
-        dispatch(fetchDocumentFailure(error));
-      },
-    );
-  };
-};
-
-// delete document from session
-export const deleteDocumentSession = () => {
-  localStorage.removeItem('activeDocument');
-  return { type: DELETE_DOCUMENT_SESSION };
-};
-
-export const deleteDocument = (idDocument) => {
-  function deleteSelectedDocument() { return { type: DELETE_DOCUMENT }; }
-  function deleteSelectedDocumentSuccess(idDocumentRemoved) { return { type: DELETE_DOCUMENT_SUCCESS, idDocumentRemoved }; }
-  function deleteSelectedDocumentFailure(error) { return { type: DELETE_DOCUMENT_FAILURE, error }; }
-  return (dispatch) => {
-    dispatch(deleteSelectedDocument(idDocument));
-    return documentService.deleteDocument(idDocument)
+    dispatch(deleteSelectedHeader(idHeader));
+    return headerService.deleteHeader(idHeader)
       .then(
-        (idDocumentRemoved) => {
-          dispatch(deleteSelectedDocumentSuccess(idDocumentRemoved));
+        (idHeaderRemoved) => {
+          dispatch(deleteSelectedHeaderSuccess(idHeaderRemoved));
         },
         (error) => {
-          dispatch(deleteSelectedDocumentFailure(error));
+          dispatch(deleteSelectedHeaderFailure(error));
         },
       );
-  };
-};
-
-export const downloadDocument = (documentId, docName, answer) => {
-  const downloadSelectedDocument = documentName => ({ type: DOWNLOAD_DOCUMENT, documentName });
-  const downloadSelectedDocumentSuccess = () => ({ type: DOWNLOAD_DOCUMENT_SUCCESS });
-  const downloadSelectedDocumentFailure = error => ({ type: DOWNLOAD_DOCUMENT_FAILURE, error });
-
-  return (dispatch, getState) => {
-    if (getState().document.isDownloadingDocument) {
-      return 1;
-    }
-    dispatch(downloadSelectedDocument(documentId));
-    return documentService.downloadDocument(documentId, docName, answer)
-      .then(response => response.blob()).then((blob) => {
-        FileSaver.saveAs(blob, `${docName}.docx`);
-        dispatch(downloadSelectedDocumentSuccess());
-      },
-      (error) => {
-        dispatch(downloadSelectedDocumentFailure(error));
-      });
   };
 };
