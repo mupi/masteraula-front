e8c8389c2924d597e9f292e4851dae405bd670e7
pamelars86 <pamela.rosales@pucp.pe>
2018-11-29 13:17:26 -0200

    close sidebar after clicks on 'Criar prova' from sidebar

--- src/containers/CreateDocumentModalContainer.js
+++ src/containers/CreateDocumentModalContainer.js
@@ -3,11 +3,14 @@ import CreateDocumentModal from 'components/document/CreateDocumentModal';
 import {
   toggleModal, createDocument,
 } from 'actions/documentAction';
+import { openSidebar } from 'actions/menuAction';
 
 // state.<reducer's name>.<property>
 const mapStateToProps = state => ({
   modal: state.document.modal,
   willAddQuestion: state.document.willAddQuestion,
+  isOpenSidebar: state.menu.isOpenSidebar,
+
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -16,6 +19,7 @@ const mapDispatchToProps = dispatch => ({
     dispatch(createDocument(formData));
     dispatch(toggleModal(modal));
   },
+  openSidebar: isOpenSidebar => dispatch(openSidebar(isOpenSidebar)),
 });
 
 const CreateDocumentModalContainer = connect(
