4339034bc37fe8cd0776dd1a3c8301076843222a
pamelars86 <pamela.rosales@pucp.pe>
2019-03-11 13:34:01 -0300

    eslint fixes

--- src/pages/UserRegister/RegisterPage.js
+++ src/pages/UserRegister/RegisterPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import RegisterForm from 'components/userregister/RegisterForm.js';
+import RegisterForm from 'components/userregister/RegisterForm';
 
 
 const RegisterPage = () => (
-<div className="main-contenedor middle-box text-center loginscreen  animated fadeInDown">
-                <div className="contenedor-register">
-                    <h3 className="text-center">Cadastre-se</h3>
-                    <RegisterForm />
-                </div>
-            </div>
-)export default RegisterPage;
+  <div className="main-contenedor middle-box text-center loginscreen  animated fadeInDown">
+    <div className="contenedor-register">
+      <h3 className="text-center">Cadastre-se</h3>
+      <RegisterForm />
+    </div>
+  </div>
+);
+export default RegisterPage;
