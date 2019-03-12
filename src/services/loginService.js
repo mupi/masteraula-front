28995c8e5ad7c8e04ecdcc2bc439b0c1797af385
pamelars86 <pamela.rosales@pucp.pe>
2019-03-12 13:58:48 -0300

    portuguese messages come from back

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -10,9 +10,9 @@ export const handleResponse = response => response.json().then((data) => {
       logout();
     }
     if (data && data.non_field_errors) {
-      if (data.non_field_errors[0] === 'E-mail is not verified.') {
+      if (data.non_field_errors[0].includes('Email ainda não verificado')) {
         return Promise.reject('Seu cadastro ainda não foi confirmado. Para enviar um novo email de confirmação, clique aqui.');
-      } if (data.non_field_errors[0] === 'Impossível fazer login com as credenciais fornecidas.') {
+      } if (data.non_field_errors[0].includes('Impossível fazer login com as credenciais fornecidas')) {
         return Promise.reject('Usuário e/ou senha inválido(s)');
       }
     }
