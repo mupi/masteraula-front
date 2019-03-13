92487d5d70ba2bc1c806986a77d5fe6dc1d71f46
pamelars86 <pamela.rosales@pucp.pe>
2019-03-13 12:08:17 -0300

    starting

--- src/services/forgotPasswordService.js
+++ src/services/forgotPasswordService.js
@@ -37,6 +37,7 @@ function resetForgotPassword(new_password1, new_password2, uid, token) {
     return response.json().then((data) => {
       if (!response.ok) {
         //const error = (data && data.error) || response.statusText;
+        
         return Promise.reject('A URL para redefinição de senha é inválida ou expirou');
       }
 
