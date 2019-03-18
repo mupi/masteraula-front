8c83c811b22f6ea809ed319fd597fc554838b7ca
pamelars86 <pamela.rosales@pucp.pe>
2019-03-18 15:38:51 -0300

    Updates

---
+++ precache-manifest.ada08d3683dd491eb3bff46ce4eaa4c6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "87bacab469a1bec33d109a6649bb1ec5",
+    "url": "/static/media/logo_masteraula-fd-verde.87bacab4.png"
+  },
+  {
+    "revision": "675e9940385cfde5f3f1",
+    "url": "/static/css/main.37db4dfa.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5e04d3019dfb3274ff31de6e268a3166",
+    "url": "/static/media/sidebar-background-2.5e04d301.jpg"
+  },
+  {
+    "revision": "dc25eca5322c4544334b",
+    "url": "/static/js/2.00335569.chunk.js"
+  },
+  {
+    "revision": "adfc9d1c1bee20580e77c4c93dc2ef6a",
+    "url": "/static/media/avataruser3.adfc9d1c.png"
+  },
+  {
+    "revision": "d8d94d4c60100b2d0344152108817820",
+    "url": "/static/media/clock.d8d94d4c.png"
+  },
+  {
+    "revision": "b1225a53dbba5758eb500953f5e4750c",
+    "url": "/static/media/search.b1225a53.png"
+  },
+  {
+    "revision": "0e5d6fa02338f5fd8429b9c371b6a14b",
+    "url": "/static/media/money.0e5d6fa0.png"
+  },
+  {
+    "revision": "675e9940385cfde5f3f1",
+    "url": "/static/js/main.a4a6ea51.chunk.js"
+  },
+  {
+    "revision": "f135e7e8de776e355a859d290bab0d90",
+    "url": "/static/media/logo_masteraula-rubrica-blanca.f135e7e8.png"
+  },
+  {
+    "revision": "2f9bf3ac729a1fba0b8a48e5239cdd6b",
+    "url": "/static/media/question-card.2f9bf3ac.jpg"
+  },
+  {
+    "revision": "42790791541df4e419590982c4c1052e",
+    "url": "/static/media/masteraula-300x60.42790791.png"
+  },
+  {
+    "revision": "4c0a171dcbdbf9566c6af0250736c980",
+    "url": "/static/media/coruja-avatar.4c0a171d.png"
+  },
+  {
+    "revision": "dc132bac960a37c790aebfb9ef2d90aa",
+    "url": "/static/media/logo_masteraula.dc132bac.png"
+  },
+  {
+    "revision": "ef2b8468921feae7df9181301c8575c1",
+    "url": "/static/media/banner.ef2b8468.jpg"
+  },
+  {
+    "revision": "dc25eca5322c4544334b",
+    "url": "/static/css/2.e7369806.chunk.css"
+  },
+  {
+    "revision": "427c1089f519b720e147c032ccb2b5ab",
+    "url": "/index.html"
+  }
+];
