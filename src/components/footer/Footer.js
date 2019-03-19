4f0f667770e0799d645c0235483af0464dfeec90
pamelars86 <pamela.rosales@pucp.pe>
2019-03-19 09:26:50 -0300

    deploy test

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = ({ year, version }) => (
         ©
         {year}
         {' '}
-        Masteraula - Mupi - versão
+        Masteraula - Mupi VF - versão
         {' '}
         {version}
       </Col>
