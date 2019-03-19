973053f010fb2beed5b63ba29a14d0e422205a22
pamelars86 <pamela.rosales@pucp.pe>
2019-03-18 23:17:41 -0300

    v2

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = ({ year, version }) => (
         ©
         {year}
         {' '}
-        Masteraula - Mupi - versão
+        Masteraula - Mupi v2 - versão
         {' '}
         {version}
       </Col>
