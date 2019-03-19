9ef850d5cf521144a78614a4c79eebb97be8c16a
pamelars86 <pamela.rosales@pucp.pe>
2019-03-19 00:47:22 -0300

    unregister v1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { unregister } from './registerServiceWorker';
 
 import configureStore from './Store';
-import * as serviceWorker from './registerServiceWorker';
+//import * as serviceWorker from './registerServiceWorker';
 
 
 const store = configureStore();
@@ -19,10 +19,9 @@ render(
   document.getElementById('root'),
 );
 
-//registerServiceWorker();
+unregister();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 //serviceWorker.register({skipWaiting : false});
-serviceWorker.unregister();
