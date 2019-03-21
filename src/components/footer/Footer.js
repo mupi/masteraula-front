133cff4f8b4e1392a32fc7e466c7e5c7b47ddf59
pamelars86 <pamela.rosales@pucp.pe>
2019-03-21 15:18:56 -0300

    Footer has contact info as email and whatsapp number. Whatsapp icon was imported from free-brands-svg-icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = ({ year, version }) => (
   <div className="c-footer text-center">
-    <Row>
+    <Row className="c-footer__section-info-contact">
       <Col xs="12">
-
-              ©
+        <FontAwesomeIcon
+          className="btn__icon"
+          icon="envelope"
+        />
+        <span>contato@masteraula.com.br</span>
+        {' | '}
+        {' '}
+        <FontAwesomeIcon
+          className="btn__icon"
+          icon={['fab', 'whatsapp']}
+        />
+        <span>(19) 997702004</span>
+      </Col>
+    </Row>
+    <Row className="c-footer__section-copyright">
+      <Col xs="12">
+        ©
+        {' '}
         {year}
         {' '}
-        MasterAula - Mupi - versão
+        Masteraula - Mupi - versão
         {' '}
         {version}
       </Col>
