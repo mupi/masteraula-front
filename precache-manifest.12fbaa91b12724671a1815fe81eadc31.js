35b41092157e78b62f855bedd63e69cb98bc4952
Daniela Palumbo <d166301@dac.unicamp.br>
2019-03-26 14:21:29 -0300

    Updates

---
+++ precache-manifest.12fbaa91b12724671a1815fe81eadc31.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "87bacab469a1bec33d109a6649bb1ec5",
+    "url": "/static/media/logo_masteraula-fd-verde.87bacab4.png"
+  },
+  {
+    "revision": "5378cc8b96a4d36d60a6",
+    "url": "/static/css/main.014fc9a4.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ef2b8468921feae7df9181301c8575c1",
+    "url": "/static/media/banner.ef2b8468.jpg"
+  },
+  {
+    "revision": "49b95aa5b994807c2296",
+    "url": "/static/js/2.c56e0d66.chunk.js"
+  },
+  {
+    "revision": "adfc9d1c1bee20580e77c4c93dc2ef6a",
+    "url": "/static/media/avataruser3.adfc9d1c.png"
+  },
+  {
+    "revision": "d8d94d4c60100b2d0344152108817820",
+    "url": "/static/media/clock.d8d94d4c.png"
+  },
+  {
+    "revision": "b1225a53dbba5758eb500953f5e4750c",
+    "url": "/static/media/search.b1225a53.png"
+  },
+  {
+    "revision": "0e5d6fa02338f5fd8429b9c371b6a14b",
+    "url": "/static/media/money.0e5d6fa0.png"
+  },
+  {
+    "revision": "5378cc8b96a4d36d60a6",
+    "url": "/static/js/main.4374debb.chunk.js"
+  },
+  {
+    "revision": "f135e7e8de776e355a859d290bab0d90",
+    "url": "/static/media/logo_masteraula-rubrica-blanca.f135e7e8.png"
+  },
+  {
+    "revision": "2f9bf3ac729a1fba0b8a48e5239cdd6b",
+    "url": "/static/media/question-card.2f9bf3ac.jpg"
+  },
+  {
+    "revision": "42790791541df4e419590982c4c1052e",
+    "url": "/static/media/masteraula-300x60.42790791.png"
+  },
+  {
+    "revision": "4c0a171dcbdbf9566c6af0250736c980",
+    "url": "/static/media/coruja-avatar.4c0a171d.png"
+  },
+  {
+    "revision": "dc132bac960a37c790aebfb9ef2d90aa",
+    "url": "/static/media/logo_masteraula.dc132bac.png"
+  },
+  {
+    "revision": "5e04d3019dfb3274ff31de6e268a3166",
+    "url": "/static/media/sidebar-background-2.5e04d301.jpg"
+  },
+  {
+    "revision": "49b95aa5b994807c2296",
+    "url": "/static/css/2.aa73f2f8.chunk.css"
+  },
+  {
+    "revision": "f011ec471e80ba183b37376230d7ef95",
+    "url": "/index.html"
+  }
+];
