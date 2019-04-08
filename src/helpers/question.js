5a4f7ffed3cd5744a430f3f3239059990ee36057
pamelars86 <pamela.rosales@pucp.pe>
2019-04-08 12:38:37 -0300

    rules css

--- src/helpers/question.js
+++ src/helpers/question.js
@@ -21,7 +21,7 @@ export const getCleanCompleteStatement = (html) => {
   const statement = sanitizeHtml(html, {
     allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
       'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-      'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe', 'img', 'span', 'sup', 'sub', 'small'],
+      'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe', 'img', 'span', 'sup', 'sub', 'small', 'u'],
     allowedClasses: {
       p: ['text-center', 'text-justify'],
       div: ['text-center', 'text-justify'],
