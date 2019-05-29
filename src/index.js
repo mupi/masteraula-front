dccfd1c95c5adc984620ada8c69ada0cea59c6a9
pamelars86 <pamela.rosales@pucp.pe>
2019-05-29 11:40:41 -0300

    register new serviceWorker - create-react-app v2

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import { ConnectedRouter } from 'connected-react-router';
 import { history } from 'helpers/history';
 import App from './App';
 
+// Create Reactp App v1
 // import registerServiceWorker from './registerServiceWorker';
 // import { unregister } from './registerServiceWorker';
 
 import configureStore from './Store';
+
+// Create Reactp App v2
 import * as serviceWorker from './serviceWorker';
 
 
@@ -27,4 +30,8 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create React App v2
+serviceWorker.register();
+
+// Create React App v1
+// registerServiceWorker();
