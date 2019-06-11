37cb58a6a35ae7e7b2e0273bc6abbaee7b30540d
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-06-11 12:21:34 -0300

    Add universal toast container

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ import { history } from 'helpers/history';
 
 import Footer from 'components/footer/Footer';
 // CSS imported in a single place (here)
+import 'react-toastify/dist/ReactToastify.css';
 import 'assets/scss/styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -61,6 +62,8 @@ import {
   faQuestionCircle,
   faLock,
 } from '@fortawesome/free-solid-svg-icons';
+
+import { ToastContainer } from 'react-toastify';
 
 import ModalRoot from './ModalRoot';
 
@@ -142,6 +145,7 @@ class App extends Component {
             <Footer year="2019" version="1.0" />
           </div>
         </LastLocationProvider>
+        <ToastContainer hideProgressBar position="bottom-right" />
       </Router>
     );
   }
