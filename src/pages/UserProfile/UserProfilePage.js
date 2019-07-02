52a5cf060436082079b55f6b5b60871808846022
Daniela Palumbo <d166301@dac.unicamp.br>
2019-07-02 16:50:19 -0300

    Vincular profile section

--- src/pages/UserProfile/UserProfilePage.js
+++ src/pages/UserProfile/UserProfilePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert } from 'reactstrap';
+import { Alert, Row, Col } from 'reactstrap';
 import UserProfileContainer from 'containers/UserProfileContainer';
 import UserPasswordProfileContainer from 'containers/UserPasswordProfileContainer';
 import FacebookLogin from 'react-facebook-login';
@@ -22,6 +22,7 @@ class UserProfilePage extends React.Component {
       return (
         <HomeUserPage>
           <Alert className="alert--warning" color="warning">
+
             Carregando ...
           </Alert>
         </HomeUserPage>
@@ -32,29 +33,42 @@ class UserProfilePage extends React.Component {
       <HomeUserPage>
         <div className="l-user-profile">
           <h3 className="text-center">
+
             Meu Perfil
           </h3>
           <h5 className="text-center">
+
             Conte um pouco sobre você
           </h5>
           <UserProfileContainer stateList={stateList} />
           <UserPasswordProfileContainer />
-        </div>
-        <FacebookLogin
-          appId={facebookLoginId}
-          fields="name,email,picture"
-          callback={responseFacebook}
-          icon="fa-facebook"
-          size="small"
-        />
-        <GoogleLogin
-          clientId={googleLoginId}
-          buttonText="Login with Google"
-          onSuccess={responseGoogle}
+          <Row style={{ marginTop: '20px' }}>
+            <Col className="text-center">
+              <span className="vertical-align">
+
+          Vincular contas
+              </span>
+            </Col>
+            <Col className="text-center">
+              <FacebookLogin
+                appId={facebookLoginId}
+                fields="name,email,picture"
+                callback={responseFacebook}
+                icon="fa-facebook"
+                size="small"
+                textButton="Entrar com Facebook"
+              />
+              <GoogleLogin
+                clientId={googleLoginId}
+                buttonText="Entrar com Google"
+                onSuccess={responseGoogle}
           // onFailure={responseGoogle}
-          cookiePolicy="single_host_origin"
-          className="google-login"
-        />
+                cookiePolicy="single_host_origin"
+                className="google-login"
+              />
+            </Col>
+          </Row>
+        </div>
       </HomeUserPage>
     );
   }
