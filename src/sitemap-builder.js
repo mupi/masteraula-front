acca582fff20a264df1c720b80f3e7ef08cf2080
pamelars86 <pamela.rosales@pucp.pe>
2019-09-12 11:34:52 -0300

    install package sitemap

---
+++ src/sitemap-builder.js
@@ -0,0 +1,12 @@
+require('babel-register');
+ 
+const router = require('./sitemap-routes.js').default;
+const Sitemap = require('react-router-sitemap').default;
+ 
+(
+    new Sitemap(<Route path='/home' />)
+        .build('https://masteraula.com.br')
+        .save('./sitemap.xml')
+);
+
+console.log("The sitemap was built."); // Only shows this message after everything works well.
