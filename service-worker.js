5033b0eef0bb122d839a1493dcf93bbf0f2c7ddb
pamelars86 <pamela.rosales@pucp.pe>
2019-09-18 09:27:36 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.335fb39b01106eeb354e6d6092c703ea.js"
+  "https://masteraula.com.br/precache-manifest.fc168dbd4ff536467ba513c4a3fa002e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://masteraula.com.br/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
