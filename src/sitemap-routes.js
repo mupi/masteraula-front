f1499828453a5cde17b025df17662420bd571d36
pamelars86 <pamela.rosales@pucp.pe>
2019-09-19 12:46:59 -0300

    sitemap created with 'public' options

--- src/sitemap-routes.js
+++ src/sitemap-routes.js
@@ -5,21 +5,12 @@ export default (
   <Route>
     <Route path="/question-base/:page(\d+)" />
     <Route path="/view-question/:id" />
-    <Route path="/classify-question/:id" />
-    <Route path="/edit-question/:id" />
     <Route path="/create-question/" />
     <Route path="/user-profile" />
     <Route path="/documents/:page(\d+)" />
-    <Route path="/my-headers/:page(\d+)" />
-    <Route path="/edit-document" />
     <Route path="/view-object/:id" />
-    <Route path="/edit-header/:id" />
-    <Route path="/new-header" />
     <Route path="/object-base/:page(\d+)" />
     <Route path="/nossos-planos" />
-    <Route path="/esqueci-senha" />
-    <Route path="/redefine-senha/:uid/:token" />
     <Route path="/terms-use" />
-    <Route path="/verify-userregister/:key" />
   </Route>
 );
