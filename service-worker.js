721f8a3d7a142693fb9dbe8a45cbd893405717f0
pamelars86 <pamela.rosales@pucp.pe>
2019-09-18 22:29:06 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa9c41e605512f5f40eeb0abe1e0b571.js"
+  "/precache-manifest.d79c937fea52a885b91374703c7a9c97.js"
 );
 
 workbox.clientsClaim();
