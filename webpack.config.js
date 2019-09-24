b2189f1668610d04f2b3643d0f93dd465d8e74a1
pamelars86 <pamela.rosales@pucp.pe>
2019-09-24 13:08:16 -0300

    remove sitemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const webpack = require("webpack");
+/*const webpack = require("webpack");
 
 module.exports = function () {
   return {
@@ -24,4 +24,4 @@ module.exports = function () {
       }
     }
   };
-};
+};*/
