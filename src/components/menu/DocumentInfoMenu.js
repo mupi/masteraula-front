3ea657d7414f934535a6b18c2362fab521ffcbf5
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-09-27 20:38:39 -0300

    Fix document info modal

--- src/components/menu/DocumentInfoMenu.js
+++ src/components/menu/DocumentInfoMenu.js
@@ -13,51 +13,26 @@ import { first5Elements } from 'helpers/document';
 class DocumentInfoMenu extends React.Component {
   constructor(props) {
     super(props);
-    this.closeModal = this.closeModal.bind(this);
+    // this.closeModal = this.closeModal.bind(this);
     this.openDocumentModal = this.openDocumentModal.bind(this);
     this.editDocument = this.editDocument.bind(this);
-    this.editDocumentFromPreview = this.editDocumentFromPreview.bind(this);
   }
 
   componentDidMount() {
-    const {
-      listMyLastDocuments, listMyDocuments,
-    } = this.props;
-    console.log("List my last docs from DocumentInfoMenu ");
+    const { listMyLastDocuments, listMyDocuments } = this.props;
 
     listMyLastDocuments(1, 'date', 'desc');
     listMyDocuments(1, 'date', 'desc');
   }
 
-  editDocument(document) {
+  switchActiveDocument(document) {
     const { switchActiveDocument } = this.props;
     switchActiveDocument(document, false);
   }
 
-  closeModal() {
-    const { hideModal } = this.props;
-    hideModal();
-  }
-
-  openDocumentModal(id) {
-    // event.preventDefault();
-    const {
-      showModal, fetchPreviewDocument, previewDocument,
-    } = this.props;
-
-    fetchPreviewDocument(parseInt(id, 10));
-    showModal({
-      open: true,
-      document: previewDocument,
-      closeModal: this.closeModal,
-      editDocument: this.editDocumentFromPreview,
-    }, 'document');
-  }
-
-  editDocumentFromPreview(document) {
-    const { switchActiveDocument } = this.props;
-    switchActiveDocument(document, true);
-    this.closeModal();
+  showDocumentModal(id) {
+    const { showDocumentModal, previewDocument } = this.props;
+    showDocumentModal(id, previewDocument);
   }
 
   render() {
@@ -94,7 +69,7 @@ class DocumentInfoMenu extends React.Component {
                   Editar
                 </DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem className="o-doc-options__dropdown-item" onClick={() => this.openDocumentModal(documentId)}>
+                <DropdownItem className="o-doc-options__dropdown-item" onClick={() => this.showDocumentModal(documentId)}>
                   <FontAwesomeIcon icon="eye" />
                   {' '}
                   Visualizar
@@ -138,7 +113,7 @@ class DocumentInfoMenu extends React.Component {
               Editar
             </Button>
           </Link>
-          <Button className="btn-margin-right menu-top__document-button" onClick={() => this.openDocumentModal(documentId)}>
+          <Button className="btn-margin-right menu-top__document-button" onClick={() => this.showDocumentModal(documentId)}>
             <FontAwesomeIcon icon="eye" className="btn__icon" />
             Visualizar
           </Button>
@@ -160,7 +135,7 @@ class DocumentInfoMenu extends React.Component {
                 <DropdownItem
                   key={document.id}
                   className="menu-top__dropdown-item"
-                  onClick={() => this.editDocument(document)}
+                  onClick={() => this.switchActiveDocument(document)}
                 >
                   {document.name}
                 </DropdownItem>
