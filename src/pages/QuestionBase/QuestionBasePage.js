4cf53f8e750f8062c4dc28bfa74dba889dd27180
pamelars86 <pamela.rosales@pucp.pe>
2019-11-19 15:58:17 -0300

    listQuestion after clicking on Search button. Fix in add discipline

--- src/pages/QuestionBase/QuestionBasePage.js
+++ src/pages/QuestionBase/QuestionBasePage.js
@@ -29,7 +29,8 @@ class QuestionBasePage extends React.Component {
     || (filter.yearsSelected !== prevProps.filter.yearsSelected)
     || (filter.topicsSelected !== prevProps.filter.topicsSelected)
     || (filter.difficultiesSelected !== prevProps.filter.difficultiesSelected)
-    || (filter.onlyMyQuestions !== prevProps.filter.onlyMyQuestions)) {
+    || (filter.onlyMyQuestions !== prevProps.filter.onlyMyQuestions)
+    || (filter.searchText !== prevProps.filter.searchText)) {
       listQuestions(parseInt(match.params.page, 10), filter, user.id);
     }
   }
