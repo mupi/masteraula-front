329d7da9c99b8cfe570e624d50075d6e4fe3fc93
pamelars86 <pamela.rosales@pucp.pe>
2019-11-28 11:03:52 -0300

    search without complete is commented

--- src/components/question/QuestionSearchText.js
+++ src/components/question/QuestionSearchText.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm, Form } from 'redux-form';
 import {
-/* Input, */ InputGroup, InputGroupAddon, Button, Row, UncontrolledTooltip,
+  /* Input, */ InputGroup, InputGroupAddon, Button, Row, UncontrolledTooltip,
 } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -9,7 +9,66 @@ import {
 } from 'helpers/validators';
 import MAAutocompleteTopics from 'components/autocomplete/MAAutocompleteTopics';
 
+/*  --Busca sem autocomplete
 const renderSearchField = ({
+  input,
+  type,
+  meta: {
+    pristine, touched, error, warning,
+  },
+  clearSearch,
+  clearSearchField,
+  search,
+  placeholder,
+  isFetchingQuestions,
+  autoFocus,
+}) => (
+  <div className="c-question-base__search-all-section">
+    <InputGroup>
+      <Input
+        {...input}
+        placeholder={placeholder}
+        type={type}
+        disabled={isFetchingQuestions}
+        className="c-question-base__search-field"
+        autoFocus={autoFocus}
+      />
+      {search || !pristine ? (
+        <InputGroupAddon addonType="prepend">
+          <Button className="c-question-base__clear-search" id="dica" onClick={search ? clearSearch : clearSearchField}>
+            <FontAwesomeIcon icon="times-circle" />
+          </Button>
+          <UncontrolledTooltip placement="bottom" target="dica" className="tooltip__message">
+                Limpar busca
+          </UncontrolledTooltip>
+        </InputGroupAddon>
+      ) : ''}
+      <InputGroupAddon addonType="prepend">
+        <Button type="submit" disabled={isFetchingQuestions}>
+          <FontAwesomeIcon icon="search" className="btn__icon" />
+                Pesquisar
+        </Button>
+      </InputGroupAddon>
+    </InputGroup>
+    { touched
+      && ((error && (
+      <span className="error-message-text">
+        {error}
+      </span>
+      ))
+      || (warning && (
+      <span>
+        {' '}
+        {warning}
+        {' '}
+      </span>
+      )))
+    }
+  </div>
+);
+*/
+
+const renderSearchFieldAutocomplete = ({
   input,
   // type,
   meta: {
@@ -93,7 +152,7 @@ class QuestionSearchText extends Component {
           Pesquisar por palavras-chave no banco de questões
           </p>
           <Field
-            component={renderSearchField}
+            component={renderSearchFieldAutocomplete}
             type="text"
             name="searchText"
             id="searchText"
