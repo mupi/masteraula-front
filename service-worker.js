bbebd6a945f3d695f35b1a6b44eacdf104cd4f6c
Glaucia S. Santos <glaucia.schnoeller@outlook.com>
2020-03-12 14:39:28 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66504956afd57b10c51b43fca747bb4c.js"
+  "/precache-manifest.a391c0e74a20c6fa0ff2578d624bd741.js"
 );
 
 workbox.clientsClaim();
