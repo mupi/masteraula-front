91797b8d04c0f47af9725017bf353b2397a6c723
Glaucia S. Santos <glaucia.schnoeller@outlook.com>
2020-07-15 18:25:54 -0300

    New component for material

--- src/pages/LearningObject/ViewLearningObjectPage.js
+++ src/pages/LearningObject/ViewLearningObjectPage.js
@@ -10,6 +10,7 @@ import LearningObjectContent from 'components/learningObject/LearningObjectConte
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import QuestionList from 'components/question/QuestionList';
 import ActivityList from 'components/activity/ActivityList';
+import MaterialList from 'components/material/MaterialList';
 import BackUsingHistory from 'components/question/BackUsingHistory';
 import { Link } from 'react-router-dom';
 
@@ -115,18 +116,13 @@ class ViewLearningObjectPage extends Component {
         <Row className="pagination-questions" style={{ marginLeft: '80%' }} />
         {activeLearningObject.questions.length > 0 || activeLearningObject.activities.length > 0 ? (
           <div className="c-question-base__results">
-            <QuestionList
+            <MaterialList
               sm="4"
               questions={activeLearningObject.questions}
               count={activeLearningObject.questions.length + activeLearningObject.activities.length}
               textResult="Materiais associados a este objeto"
-              showLink={false}
-              {...this.props}
-            />
-            <ActivityList 
-              sm="4"
               activities={activeLearningObject.activities} 
-              showQuantity={false}
+              showQuantity={true}
               {...this.props}
             />
           </div>
