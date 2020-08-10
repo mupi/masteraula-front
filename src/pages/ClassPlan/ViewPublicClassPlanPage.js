b1f21009688b46a4dfb62b677d6a02243ee0ef1c
pame <pamela.rosales@pucp.pe>
2020-08-10 13:57:43 -0300

    public class plan with stations done

--- src/pages/ClassPlan/ViewPublicClassPlanPage.js
+++ src/pages/ClassPlan/ViewPublicClassPlanPage.js
@@ -12,6 +12,7 @@ import BackUsingHistory from 'components/question/BackUsingHistory';
 import PublicActivityContentList from 'components/activity/PublicActivityContentList';
 import URLCopy from 'components/onlineTest/URLCopy';
 import { masteraulaUrl } from 'helpers/config';
+import { getCleanCompleteStatement } from 'helpers/question';
 
 const MenuAdminOptions = () => (
   <Row className="c-online__row-header-options c-online__row-header-options--fixed">
@@ -27,11 +28,6 @@ const CLASSPLAN_TYPE = {
   STATIONS: 'S',
   OPEN: 'T', /* before traditional, now Open */
 };
-
-const PublicClassPlanStations = ({ classPlan }) => (
-  <div>{classPlan.name}</div>
-);
-
 const PublicActivitiesSection = ({ activities }) => (
   <>
     <Row className="mb-2">
@@ -52,7 +48,7 @@ const PublicOnlineTestSection = ({ onlineTests }) => (
     <Row className="mb-2">
       <Col sm="12">
         <h5>
-          <FontAwesomeIcon icon="book-reader" />
+          <FontAwesomeIcon icon="laptop" />
           {' '}
           Provas online
         </h5>
@@ -62,7 +58,7 @@ const PublicOnlineTestSection = ({ onlineTests }) => (
       <Row className="mb-2" key={ot.link}>
         <Col sm="12">
           <div className="my-2">
-            <URLCopy url={`${masteraulaUrl}/view-public-classplan/${ot.link}`} />
+            <URLCopy url={`${masteraulaUrl}/apply-online/${ot.link}`} />
           </div>
         </Col>
       </Row>
@@ -70,13 +66,77 @@ const PublicOnlineTestSection = ({ onlineTests }) => (
   </>
 );
 
+const PublicStationsSection = ({
+  stations,
+}) => (
+  <>
+    <Row className="c-question__tittle-section">
+      <Col>
+        <h5>
+          <FontAwesomeIcon icon="sync-alt" />
+          {' '}
+          Estações
+        </h5>
+        <div className="border-top my-3" />
+      </Col>
+    </Row>
+    <div className="c-classplan__stations">
+      { stations && stations.map((station, i) => (
+        <div className="c-classplan__view-station border-bottom my-3" key={station.id}>
+          <PublicSingleStation
+            station={station}
+            position={i}
+          />
+        </div>
+      )) }
+    </div>
+  </>
+);
+
+/* eslint-disable react/no-danger */
+const PublicSingleStation = ({
+  station, position,
+}) => (
+  <>
+    <Row>
+      <Col sm="12">
+        <h6><strong>{`Estação ${position + 1}`}</strong></h6>
+      </Col>
+    </Row>
+    <Row>
+      <Col sm="12">
+        <div dangerouslySetInnerHTML={{ __html: `Nome: ${getCleanCompleteStatement(station.name_station)}` }} />
+      </Col>
+    </Row>
+    <Row className="mb-3 align-items-center">
+      <Col sm="12" xs="12">
+        <div dangerouslySetInnerHTML={{ __html: getCleanCompleteStatement(station.description_station) }} />
+      </Col>
+      <Col sm="12" xs="12">
+        {
+            station.activity
+            && (<PublicActivitiesSection activities={[{ ...station.activity }]} />)
+          }
+        {
+            station.document_online
+            && (<PublicOnlineTestSection onlineTests={[{ ...station.document_online }]} />)
+        }
+      </Col>
+    </Row>
+  </>
+);
+
 const PublicOpenClassPlan = ({ classPlan }) => (
   <>
+    <ClassPlanStudentArea classPlan={classPlan} />
     {classPlan.activities && <PublicActivitiesSection activities={classPlan.activities} />}
     {classPlan.documents_online && <PublicOnlineTestSection onlineTests={classPlan.documents_online} />}
   </>
 );
 
+const PublicClassPlanStations = ({ classPlan }) => (
+  <div>{classPlan.stations && <PublicStationsSection stations={classPlan.stations} />}</div>
+);
 
 const InnerPage = (props) => {
   const {
@@ -126,7 +186,6 @@ const InnerPage = (props) => {
           {activePublicClassPlan.name}
         </Col>
       </Row>
-      <ClassPlanStudentArea classPlan={activePublicClassPlan} />
       {activePublicClassPlan.plan_type === CLASSPLAN_TYPE.OPEN
         ? <PublicOpenClassPlan classPlan={activePublicClassPlan} />
         : <PublicClassPlanStations classPlan={activePublicClassPlan} />
